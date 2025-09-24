86d5842eb74f8035a0f7062296d281190833f596
dngml11 <dngml11@gmail.com>
2025-09-24 13:32:10 +0900

    크롤링 연습3

---
+++ web2.py
@@ -0,0 +1,31 @@
+# web2.py
+# 클리앙 웹사이트 중고장터
+from bs4 import BeautifulSoup
+import urllib.request
+import re
+
+#파일에 저장
+f= open('clien.txt','w',encoding='utf-8')
+
+
+url="https://www.clien.net/service/board/sold"
+#검색이 용이한 객체 생성
+data=urllib.request.urlopen(url)
+#스프 객체 생성
+soup=BeautifulSoup(data,'html.parser')
+
+# 선택한 블럭 주석 : ctrl + /
+# <span class="subject_fixed" data-role="list-title-text" title="용인) 아이폰 15 프로 맥스 1테라 화이트 팝니다">
+# 							용인) 아이폰 15 프로 맥스 1테라 화이트 팝니다
+# 						</span>
+
+#중고장터 매물 제목
+list=soup.find_all('span',attrs={'data-role':'list-title-text'})
+for item in list:
+    #문자열 가공
+    title=item.text.strip()
+    print(title)
+    f.write(title+'\n')
+
+#1열에서 코딩
+f.close()
